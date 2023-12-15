58cbf8635173649132bc161bcf3b853efb50609f
Isaac <icorbrey@gmail.com>
2023-12-15 17:56:22 -0500

    wip

--- src/aoc_2023/day_03_gear_ratios.rs
+++ src/aoc_2023/day_03_gear_ratios.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 use crate::utils::prelude::*;
 
 pub struct GearRatios;
@@ -8,10 +10,189 @@ impl Problem for GearRatios {
     }
 
     fn part_1(&self, input: &str) {
-        todo!()
+        let globs = load_globs(input);
+
+        let symbols: Vec<Symbol> = globs.iter().filter_map(Glob::symbol).collect();
+        let numbers: Vec<Number> = globs.iter().filter_map(Glob::number).collect();
+
+        let sum: u32 = numbers
+            .iter()
+            .filter(|n| symbols.iter().any(|s| s.bounds.is_adjacent_to(&n.bounds)))
+            .map(|n| n.value)
+            .sum();
+
+        println!("Sum: {}", sum);
     }
 
     fn part_2(&self, input: &str) {
-        todo!()
+        let globs = load_globs(input);
+
+        let symbols: Vec<Symbol> = globs.iter().filter_map(Glob::symbol).collect();
+        let numbers: Vec<Number> = globs.iter().filter_map(Glob::number).collect();
+
+        let sum: u32 = symbols
+            .iter()
+            .filter(|s| s.value == '*')
+            .map(|s| {
+                numbers
+                    .iter()
+                    .filter(|n| s.bounds.is_adjacent_to(&n.bounds))
+                    .map(|n| n.value)
+                    .collect::<Vec<u32>>()
+            })
+            .filter(|nums| nums.len() == 2)
+            .map(|nums| nums.iter().product::<u32>())
+            .sum();
+
+        println!("Sum: {}", sum);
+    }
+}
+
+fn load_globs(input: &str) -> Vec<Glob> {
+    input
+        .split('\n')
+        .enumerate()
+        .flat_map(Glob::from_line)
+        .collect()
+}
+
+enum Glob {
+    Number(Number),
+    Symbol(Symbol),
+}
+
+const MATCH_NUMBER: &'static str = r"\d+";
+const MATCH_SYMBOL: &'static str = r"[\*\$\+\-\=\/\@\%\&\#]";
+
+impl Glob {
+    pub fn from_line((y, line): (usize, &str)) -> Vec<Glob> {
+        let mut globs = vec![];
+        if let Ok(re) = Regex::new(MATCH_NUMBER) {
+            let mut numbers = re
+                .find_iter(line)
+                .map(|c| {
+                    Glob::Number(Number {
+                        value: c.as_str().parse().unwrap(),
+                        bounds: Bounds {
+                            x: c.start(),
+                            l: c.len(),
+                            y,
+                        },
+                    })
+                })
+                .collect::<Vec<Glob>>();
+            globs.append(&mut numbers);
+        }
+        if let Ok(re) = Regex::new(MATCH_SYMBOL) {
+            let mut symbols = re
+                .find_iter(line)
+                .map(|c| {
+                    Glob::Symbol(Symbol {
+                        value: c.as_str().chars().next().unwrap(),
+                        bounds: Bounds {
+                            x: c.start(),
+                            l: c.len(),
+                            y,
+                        },
+                    })
+                })
+                .collect::<Vec<Glob>>();
+            globs.append(&mut symbols);
+        }
+        globs
+    }
+
+    pub fn number(&self) -> Option<Number> {
+        match self {
+            Glob::Number(value) => Some(*value),
+            _ => None,
+        }
+    }
+
+    pub fn symbol(&self) -> Option<Symbol> {
+        match self {
+            Glob::Symbol(value) => Some(*value),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+struct Number {
+    value: u32,
+    bounds: Bounds,
+}
+
+#[derive(Clone, Copy)]
+struct Symbol {
+    value: char,
+    bounds: Bounds,
+}
+
+#[derive(Clone, Copy)]
+struct Bounds {
+    x: usize,
+    y: usize,
+    l: usize,
+}
+
+impl Bounds {
+    pub fn is_adjacent_to(&self, other: &Self) -> bool {
+        (self.x - other.l <= other.x && other.x <= self.x + self.l)
+            && (self.y - 1 <= other.y && other.y <= self.y + 1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn tests_bounds_adjacency() {
+        assert!(Bounds { x: 1, y: 1, l: 1 }.is_adjacent_to(&Bounds { x: 0, y: 1, l: 1 }));
+        assert!(Bounds { x: 1, y: 1, l: 1 }.is_adjacent_to(&Bounds { x: 1, y: 1, l: 1 }));
+        assert!(Bounds { x: 1, y: 1, l: 1 }.is_adjacent_to(&Bounds { x: 2, y: 1, l: 1 }));
+        assert!(!Bounds { x: 1, y: 1, l: 1 }.is_adjacent_to(&Bounds { x: 3, y: 1, l: 1 }));
+    }
+
+    #[test]
+    fn creates_numbers_from_strings() {
+        let globs = Glob::from_line((0, ""));
+
+        assert_eq!(0, globs.len());
+
+        let globs = Glob::from_line((67, "12....345..*/"));
+
+        assert_eq!(4, globs.len());
+        assert_eq!(2, globs.iter().filter_map(Glob::number).count());
+        assert_eq!(2, globs.iter().filter_map(Glob::symbol).count());
+
+        let number_1 = globs[0].number().unwrap();
+
+        assert_eq!(12, number_1.value);
+        assert_eq!(0, number_1.bounds.x);
+        assert_eq!(67, number_1.bounds.y);
+        assert_eq!(2, number_1.bounds.l);
+
+        let number_2 = globs[1].number().unwrap();
+
+        assert_eq!(345, number_2.value);
+        assert_eq!(6, number_2.bounds.x);
+        assert_eq!(67, number_2.bounds.y);
+        assert_eq!(3, number_2.bounds.l);
+
+        let symbol_1 = globs[2].symbol().unwrap();
+
+        assert_eq!('*', symbol_1.value);
+        assert_eq!(11, symbol_1.bounds.x);
+        assert_eq!(67, symbol_1.bounds.y);
+        assert_eq!(1, symbol_1.bounds.l);
+
+        let symbol_2 = globs[3].symbol().unwrap();
+
+        assert_eq!('/', symbol_2.value);
+        assert_eq!(12, symbol_2.bounds.x);
+        assert_eq!(67, symbol_2.bounds.y);
+        assert_eq!(1, symbol_2.bounds.l);
     }
 }
--- src/aoc_2023/day_04_scratchcards.rs
+++ src/aoc_2023/day_04_scratchcards.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 use crate::utils::prelude::*;
 
 pub struct Scratchcards;
@@ -8,10 +10,128 @@ impl Problem for Scratchcards {
     }
 
     fn part_1(&self, input: &str) {
-        todo!()
+        let sum: u32 = load_cards(input).iter().map(Card::points).sum();
+        println!("Sum: {}", sum);
     }
 
     fn part_2(&self, input: &str) {
-        todo!()
+        let mut cards = load_cards(input);
+
+        for i in 0..cards.len() {
+            let _cards = cards.clone();
+            let cursor = _cards.get(i).unwrap();
+            for j in (i + 1)..(cursor.matches() as usize + i + 1) {
+                cards.get_mut(j).and_then(|c| Some(c.add(cursor.count)));
+            }
+        }
+
+        let sum: u32 = cards.iter().map(|c| c.count).sum();
+        println!("Sum: {}", sum);
+    }
+}
+
+fn load_cards(input: &str) -> Vec<Card> {
+    input
+        .split('\n')
+        .filter(|l| l.len() != 0)
+        .map(Card::from)
+        .collect()
+}
+
+#[derive(Debug, PartialEq)]
+struct Card {
+    pub id: u32,
+    pub numbers: Vec<u32>,
+    pub winning_numbers: Vec<u32>,
+    pub count: u32,
+}
+
+impl Card {
+    pub fn matches(&self) -> u32 {
+        (self.numbers.iter())
+            .filter(|x| self.winning_numbers.contains(x))
+            .count() as u32
+    }
+
+    pub fn points(&self) -> u32 {
+        match self.matches() {
+            0 => 0,
+            x => 2_u32.pow(x - 1),
+        }
+    }
+
+    pub fn add(&mut self, quantity: u32) {
+        self.count += quantity;
+    }
+}
+
+impl Clone for Card {
+    fn clone(&self) -> Self {
+        Self {
+            id: self.id.clone(),
+            numbers: self.numbers.clone(),
+            winning_numbers: self.winning_numbers.clone(),
+            count: self.count.clone(),
+        }
+    }
+}
+
+const MATCH_CARD: &'static str = r"Card\s+(\d+):\s+((?:\d+\s+)+)\|((?:\s+\d+)+)";
+
+impl From<&str> for Card {
+    fn from(value: &str) -> Self {
+        let cap = (Regex::new(MATCH_CARD).ok())
+            .and_then(|re| re.captures(value))
+            .expect("Match was not found.");
+
+        Self {
+            id: (cap.get(1))
+                .and_then(|id| id.as_str().parse().ok())
+                .expect("ID was not a number."),
+            winning_numbers: (cap.get(2))
+                .expect("Winning numbers were not found.")
+                .as_str()
+                .replace("  ", " ")
+                .split(" ")
+                .filter_map(|x| x.parse().ok())
+                .collect(),
+            numbers: (cap.get(3))
+                .expect("Numbers were not found.")
+                .as_str()
+                .replace("  ", " ")
+                .split(" ")
+                .filter_map(|x| x.parse().ok())
+                .collect(),
+            count: 1,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn creates_cards_from_strings() {
+        assert_eq!(
+            Card::from("Card 123: 1 2 3 | 4 5 6"),
+            Card {
+                id: 123,
+                numbers: vec![4, 5, 6],
+                winning_numbers: vec![1, 2, 3],
+                count: 1,
+            }
+        );
+    }
+
+    #[test]
+    fn calculates_points() {
+        assert_eq!(0, Card::from("Card 1: 1 | 0").points());
+        assert_eq!(1, Card::from("Card 1: 1 | 1").points());
+        assert_eq!(2, Card::from("Card 1: 1 2 | 1 2").points());
+        assert_eq!(4, Card::from("Card 1: 1 2 3 | 1 2 3").points());
+        assert_eq!(8, Card::from("Card 1: 1 2 3 4 | 1 2 3 4").points());
+        assert_eq!(16, Card::from("Card 1: 1 2 3 4 5 | 1 2 3 4 5").points());
+        assert_eq!(32, Card::from("Card 1: 1 2 3 4 5 6 | 1 2 3 4 5 6").points());
     }
 }
